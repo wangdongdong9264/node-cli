900a5b4c6884df242a53770783eea7719b8affca
wangdongdongMac <wangdongdong9264@hotmail.com>
2020-09-02 00:40:54 +0800

    添加 alioss upload

--- packages/commands/upload.js
+++ packages/commands/upload.js
@@ -2,6 +2,7 @@
 // 后续可以考虑单例模式
 
 const qiniu = require('qiniu')
+const OSS = require('ali-oss')
 const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk') // 终端彩色输出
@@ -10,7 +11,54 @@ let UploadConfig = {}
 let url = ''
 
 function ali(config) {
-  
+  const {
+    accessKeyId,
+    accessKeySecret,
+    bucket,
+    region
+  } = config
+
+  if (accessKeyId && accessKeySecret && bucket && region) {
+    const client = new OSS({
+      bucket,
+      region,
+      accessKeyId,
+      accessKeySecret
+    })
+
+    async function put (key, file) {
+      try {
+        //object-name可以自定义为文件名（例如file.txt）或目录（例如abc/test/file.txt）的形式，实现将文件上传至当前Bucket或Bucket下的指定目录。
+        let result = await client.put(key, file)
+        console.log(result)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+    // 递归上传
+    const uploadAll = (dir, prefix) => {
+      const files = fs.readdirSync(dir)
+      files.forEach(file => {
+        const filePath = path.join(dir, file)
+        const key = prefix ? `${prefix}/${file}` : file
+        if (fs.lstatSync(filePath).isDirectory()) {
+          return uploadAll(filePath, key)
+        }
+        put(key, filePath)
+      })
+    }
+
+    // 执行
+    if (fs.existsSync(url)) {
+      uploadAll(url)
+    } else {
+      console.log(chalk.yellow(`Path does not exist ！`))
+    }
+
+  } else {
+    console.log(chalk.yellow(`Parameter error ！`))
+  }
 }
 
 function qn(config) {
@@ -72,7 +120,6 @@ function qn(config) {
 }
 
 function upload(DongdongConfig) {
-
   const {
     upload:{
       server,
